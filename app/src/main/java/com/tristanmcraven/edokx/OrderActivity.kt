57f20bd9a283943df2a38b533c4abb7f1ec8ed8a
Tristan McRaven <tristanmcraven@gmail.com>
2025-02-10 08:31:42 +0500

    dump (finish order activity)

--- app/src/main/java/com/tristanmcraven/edokx/OrderActivity.kt
+++ app/src/main/java/com/tristanmcraven/edokx/OrderActivity.kt
@@ -1,12 +1,19 @@
 package com.tristanmcraven.edokx
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class OrderActivity : AppCompatActivity() {
+
+    private lateinit var textViewOrderNumber: TextView
+    private lateinit var textViewRestName: TextView
+    private lateinit var textViewRestAddress: TextView
+    private lateinit var linearLayoutOrderContents: Linear
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +23,13 @@ class OrderActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    fun initViews() {
+
+    }
+
+    fun retrieveIntentData() {
+
     }
 }
